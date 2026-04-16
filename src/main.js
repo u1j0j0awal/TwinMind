096dbc945d2a7ea2f5efcd365694d889043e4fd0
ujjawal <ujjawalsaurav726@gmail.com>
2026-04-17 00:50:07 +0530

    Remove compiled .js files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-const root = document.getElementById('root');
-if (root) {
-    ReactDOM.createRoot(root).render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
-}
